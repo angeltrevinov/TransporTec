3d060476a4127e4f07938efb472f3ac5b5313dd3
Angel Trevino <angel.trevinov@gmail.com>
2019-04-07 20:07:24 -0500

    Including map plugin

--- Front/TransporTec/src/app/app.module.ts
+++ Front/TransporTec/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import {NgModule, NO_ERRORS_SCHEMA} from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import {SharedModule} from "~/app/shared/shared.module";
 import {DriverModule} from "~/app/driver/driver.module";
+import {platformNames} from "tns-core-modules/platform";
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+
+declare var GMSServices: any;
+if (platformNames.ios) {
+    GMSServices.provideAPIKey("AIzaSyBPVAfpJcBbty9sD3nTEaAfJoQ4GjKI4o0");
+}
+
 
 @NgModule({
     bootstrap: [
