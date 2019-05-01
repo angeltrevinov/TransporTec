c34f1b4f5176b33270ab6348c7645ffc9e93d352
Julia Jimenez <maggie.jh99@gmail.com>
2019-05-01 11:46:03 -0500

    Added qr

--- Back/Server/Controllers/usuarios.controller.js
+++ Back/Server/Controllers/usuarios.controller.js
@@ -1,9 +1,23 @@
 const Usuario = require('../Models/usuario.js');
 const usuarioCtrl = {};
+const fs = require('fs');
+const qrcode = require('qrcode');
 
 usuarioCtrl.getUsuarios = async (req, res) => {
   const usuarios = await Usuario.find();
   res.json(usuarios);
+
+run().catch(error => console.error(error.stack));
+
+
+async function run() {
+  const mags = await Usuario.find({"strNombre" : "Julia Jimenez"}, { strNombre: 1 });
+
+  const res = await qrcode.toDataURL(JSON.stringify(mags));
+
+  fs.writeFileSync('./mags.html', `<img src="${res}">`);
+  //console.log(<img src="${res}">);
+}
 }
 usuarioCtrl.createUsuario = async (req, res) =>{
 }
@@ -14,6 +28,9 @@ usuarioCtrl.editUsuario = function(){
 
 }
 usuarioCtrl.delUsuario = function(){
+
+}
+usuarioCtrl.Ticket = async (req, res) => {
 
 }
 module.exports = usuarioCtrl;
