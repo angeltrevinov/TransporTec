026840b99c25bb616fa0be1d439b060d6a6b21d9
Angel Trevino <angel.trevinov@gmail.com>
2019-05-01 23:49:18 -0500

    Adding LogInService connection to the front

---
+++ Front/TransporTec/src/app/shared/config/TransportecAPI.ts
@@ -0,0 +1,6 @@
+export class TransporTecAPI {
+
+    static BaseURL: string = 'http://localhost:3000';
+    static LogInURL: string = TransporTecAPI.BaseURL +
+        '/authentication';
+}
--- Front/TransporTec/src/app/shared/services/log-in.service.ts
+++ Front/TransporTec/src/app/shared/services/log-in.service.ts
@@ -1,18 +1,40 @@
 import { Injectable } from '@angular/core';
 import {RouterExtensions} from "nativescript-angular";
 import * as dialogs from "tns-core-modules/ui/dialogs";
+import {HttpClient} from "@angular/common/http";
+import {TransporTecAPI} from "~/app/shared/config/TransportecAPI";
 
 @Injectable()
 export class LogInService {
     //------------------------------------------------------------------------------------------------------------------
     constructor(
-        private router: RouterExtensions
+        private router: RouterExtensions,
+        private http: HttpClient
     ) { }
     //------------------------------------------------------------------------------------------------------------------
     LogIn(
         strEmail: string,
         strPassword: string
     ) {
+
+        let strUrl: string = TransporTecAPI.LogInURL;
+
+        this.http.post<object>(strUrl,
+            {
+                strCorreo: strEmail,
+                strContraseña: strPassword
+            }).subscribe(
+            (data) => {
+                console.log(data);
+            },
+            (error) => {
+                console.log(error);
+            },
+            () => {
+                //                                          //Everything is completed!
+            }
+        )
+
         if(
             //                                              //If the user is a driver send it to the driver pages
             strEmail == 'driver') {
