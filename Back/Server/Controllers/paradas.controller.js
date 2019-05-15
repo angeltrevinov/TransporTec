f850c220996d3de015515dab09ea92407302b27b
Julia Jimenez <maggie.jh99@gmail.com>
2019-05-15 16:05:36 -0500

    Commenting code

--- Back/Server/Controllers/paradas.controller.js
+++ Back/Server/Controllers/paradas.controller.js
@@ -3,13 +3,13 @@ const paradasCtrl = {};
 
 paradasCtrl.getParadas = async (req, res, next) => {
 
+    // Obtener las paradas apartir de la ruta que mandan
     strIdRuta = req.params.idRuta;
     const paradas = await Parada.find(
-        { 
+        {
             strIdRuta: strIdRuta
         }
     );
-
 
     if(!paradas){
         return res.status(400).send({
--- Back/Server/database.js
+++ Back/Server/database.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
+//link para conectarse con el cluster
 const URI = 'mongodb+srv://Maggs:Jm137924685@transportec-2wtsm.mongodb.net/Transportec?retryWrites=true';
 mongoose.connect(URI, {useNewUrlParser: true})
   .then(db => console.log('DB is connected'))
   .catch(err => console.error(err));
 
 module.exports = mongoose;
-
-
